0f7cbbc784aff0a9e9a4cbd4be3202011aa65cf0
Unknown_303YT <unknown.303yt@gmail.com>
2026-07-21 18:07:05 +0930

    Initial Server Setup. Add gradle.properties Add server/gradle.properties Add server/src/main/java/com/riverstone/unknown303/foundry/server/Main.java Add server/src/main/resources/application.properties
    Signed-off-by: Unknown_303YT <unknown.303yt@gmail.com>

---
+++ server/src/main/java/com/riverstone/unknown303/foundry/server/Main.java
@@ -0,0 +1,7 @@
+package com.riverstone.unknown303.foundry.server;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Foundry server started");
+    }
+}
