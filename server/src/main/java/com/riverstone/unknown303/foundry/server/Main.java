25c9dfb3495416c8f450efe03465c314f1d1a6d9
Unknown_303YT <unknown.303yt@gmail.com>
2026-08-24 13:56:53 +0930

    Add server Update ServerApplication.java Add HealthController.java Delete application-dev.properties Delete application-prod.properties Update server/settings.gradle
    Signed-off-by: Unknown_303YT <unknown.303yt@gmail.com>

--- server/src/main/java/com/riverstone/unknown303/foundry/server/Main.java
+++
@@ -1,7 +0,0 @@
-package com.riverstone.unknown303.foundry.server;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Foundry server started");
-    }
-}
---
+++ server/src/main/java/com/riverstone/unknown303/foundry/server/ServerApplication.java
@@ -0,0 +1,11 @@
+package com.riverstone.unknown303.foundry.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
